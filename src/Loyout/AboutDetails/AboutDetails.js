0f65dd2aaa70ff383b7b1ad64fb30e8c2d9a122b
akofisher <akolekveishvilI@gmail.com>
2023-06-14 19:04:42 +0400

    about us file functionality is ready for sending to back-end

--- src/Loyout/AboutDetails/AboutDetails.js
+++ src/Loyout/AboutDetails/AboutDetails.js
@@ -1,10 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../Img/restLogo.jpg'
 import { aboutDetails, bannerPhoto } from '../../data'
 import Loyout from '../Loyout'
 import './AboutDetails.css'
 
 export default function AboutDetails() {
+  const [file1, setFile1] = useState(null)
+  const [file2, setFile2] = useState(null)
+  const [file3, setFile3] = useState(null)
+  const [photo1, setPhoto1] = useState('')
+  const [photo2, setPhoto2] = useState('')
+  const [photo3, setPhoto3] = useState('')
+  const [aboutUsText, setAboutUsText] = useState('')
+
+  const handleFileChange1 = (event) => {
+    const file = event.target.files[0]
+    setFile1(file)
+
+    if (file) {
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const base64 = reader.result
+        setPhoto1(base64)
+      }
+
+      reader.readAsDataURL(file)
+    }
+  }
+
+  const handleFileChange2 = (event) => {
+    const file = event.target.files[0]
+    setFile2(file)
+
+    if (file) {
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const base64 = reader.result
+        setPhoto2(base64)
+      }
+
+      reader.readAsDataURL(file)
+    }
+  }
+
+  const handleFileChange3 = (event) => {
+    const file = event.target.files[0]
+    setFile3(file)
+
+    if (file) {
+      const reader = new FileReader()
+
+      reader.onload = () => {
+        const base64 = reader.result
+        setPhoto3(base64)
+      }
+
+      reader.readAsDataURL(file)
+    }
+  }
+
   const handleSubmit = () => {}
   return (
     <Loyout>
@@ -36,62 +92,61 @@ export default function AboutDetails() {
           </div>
           <div className="contact_right_container">
             <p className="contact_container_header">განაახლე დეტალები</p>
-            <form onSubmit={() => handleSubmit()} className="contact-form">
+            <form onSubmit={(e) => e.preventDefault()} className="contact-form">
               <div className="contact-inp-cont">
-                <label className="label" htmlFor="password">
+                <label className="label" htmlFor="aboutText">
                   შეიყვანეთ ჩვენს შესახებ ტექსტი
                 </label>
                 <textarea
-                  id="userName"
-                  name="userName"
+                  id="aboutText"
+                  name="aboutText"
                   type="text"
-                  onChange={(val) => console.log()}
+                  onChange={(val) => setAboutUsText(val.target.value)}
                   variant="outlined"
-                  label="მომხმარებელი"
                   className="textarea-user-input"
                 />
               </div>
               <div className="contact-inp-cont">
-                <label className="label" htmlFor="password">
+                <label className="label" htmlFor="photo1">
                   ატვირთეთ ჩვენი ფოტო N1
                 </label>
                 <input
-                  id="userName"
-                  name="userName"
+                  id="photo1"
+                  name="photo1"
                   type="file"
-                  onChange={(val) => console.log()}
+                  onChange={(val) => handleFileChange1(val.target.value)}
                   variant="outlined"
-                  label="მომხმარებელი"
                   className="user-input"
+                  accept="image/*"
                 />
               </div>
               <div className="contact-inp-cont">
-                <label className="label" htmlFor="password">
+                <label className="label" htmlFor="photo2">
                   ატვირთეთ ჩვენი ფოტო N2
                 </label>
                 <input
-                  id="userName"
-                  name="userName"
+                  id="photo2"
+                  name="photo2"
                   type="file"
-                  onChange={(val) => console.log()}
+                  onChange={(val) => handleFileChange2(val.target.value)}
                   variant="outlined"
-                  label="მომხმარებელი"
                   className="user-input"
+                  accept="image/*"
                 />
               </div>
 
               <div className="contact-inp-cont">
-                <label className="label" htmlFor="password">
+                <label className="label" htmlFor="photo3">
                   ატვირთეთ ფეიჯების ბანერი
                 </label>
                 <input
-                  id="userName"
-                  name="userName"
+                  id="photo3"
+                  name="photo3"
                   type="file"
-                  onChange={(val) => console.log()}
+                  onChange={(val) => handleFileChange3(val.target.value)}
                   variant="outlined"
-                  label="მომხმარებელი"
                   className="user-input"
+                  accept="image/*"
                 />
               </div>
 
